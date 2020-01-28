b798d6742ca1267b01c70540f85ae32582ea1652
Lenqth <nullptr00@gmail.com>
2020-01-28 13:11:18 +0900

    stylelintrcをjsに変更。ルール修正。

---
+++ workspaces/editor/.stylelintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "plugins": [
+        "stylelint-scss",
+    ],
+    "rules": {
+        "indentation": 4,
+        "linebreaks": "unix",
+        "declaration-block-no-shorthand-property-overrides": true,
+        "declaration-colon-newline-after": null,
+        "at-rule-no-unknown": [true, {
+            "ignoreAtRules": ["function", "if", "for", "each", "include", "mixin", "content"]
+        }]
+    },
+    "extends": [
+        "stylelint-config-standard"
+    ]
+}
