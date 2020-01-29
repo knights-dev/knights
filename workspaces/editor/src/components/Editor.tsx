ea549516e9ca9e85a776ee4b2a35bbd2cf1de23a
0918nobita <nobita.0918@gmail.com>
2020-01-29 13:27:19 +0900

    :sparkles: マウスドラッグでノード全体を平行移動できるようにした

--- workspaces/editor/src/components/Editor.tsx
+++ workspaces/editor/src/components/Editor.tsx
@@ -1,16 +1,70 @@
 /* @jsx jsx */
 import { jsx } from '@emotion/core';
-import React from 'react';
+import React, { createRef, useEffect } from 'react';
+import { BehaviorSubject, fromEvent } from 'rxjs';
+import { map, switchMap, takeUntil } from 'rxjs/operators';
 
 import { ArrowLine, IONode, PatternNode, ValueNode } from './nodes';
 
 export const Editor = (): JSX.Element => {
-    const viewBox = [0, 0, 800, 600];
-    const viewBoxStr = viewBox.join(' ');
+    const viewBoxInitSize = [0, 0, 800, 600];
+
+    const ref = createRef<SVGSVGElement>();
+
+    useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const svgElement = ref.current!;
+
+        const viewBox$ = new BehaviorSubject(viewBoxInitSize);
+
+        const viewboxSubscription = viewBox$.subscribe(size => {
+            svgElement.setAttribute('viewBox', size.join(' '));
+        });
+
+        const mouseDown$ = fromEvent<MouseEvent>(svgElement, 'mousedown');
+        const mouseMove$ = fromEvent<MouseEvent>(svgElement, 'mousemove');
+        const mouseUp$ = fromEvent(window, 'mouseup');
+
+        const dragSubscription = mouseDown$
+            .pipe(
+                switchMap(event => {
+                    event.preventDefault();
+
+                    let prevX = event.clientX;
+                    let prevY = event.clientY;
+
+                    return mouseMove$.pipe(
+                        map(moveEvent => {
+                            moveEvent.preventDefault();
+
+                            const delta = { dx: moveEvent.clientX - prevX, dy: moveEvent.clientY - prevY };
+
+                            prevX = moveEvent.clientX;
+                            prevY = moveEvent.clientY;
+
+                            return delta;
+                        }),
+                        takeUntil(mouseUp$)
+                    );
+                })
+            )
+            .subscribe(({ dx, dy }) => {
+                const viewBoxValues = viewBox$.getValue();
+                viewBoxValues[0] = viewBoxValues[0] - dx;
+                viewBoxValues[1] = viewBoxValues[1] - dy;
+                viewBox$.next(viewBoxValues);
+            });
+
+        return (): void => {
+            viewboxSubscription.unsubscribe();
+            dragSubscription.unsubscribe();
+        };
+    }, []);
+
     return (
         <React.Fragment>
             <div className="editor-container">
-                <svg viewBox={viewBoxStr} className="editor-screen">
+                <svg ref={ref} className="editor-screen">
                     <IONode x={50} y={50} text="Input" />
                     <PatternNode x={50} y={150} />
                     <ValueNode x={50} y={250} text="f" />
