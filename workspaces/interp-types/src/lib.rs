38882943737be012fbdd569b644b344629db4a44
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-02-08 18:45:53 +0900

    :alembic: Define AST, values and types temporary

--- workspaces/interp-types/src/lib.rs
+++ workspaces/interp-types/src/lib.rs
@@ -1,19 +1,35 @@
 //! Definitions AST, values, and types used by interp-if and interp-core
 
+type Name = String;
+type Num = f64;
+
+/// Literal expression
+#[derive(Clone, Debug)]
+pub enum Lit {
+    Num(Num),
+}
+
 /// AST
 #[derive(Clone, Debug)]
 pub enum Expr {
-    Foo,
+    App(Box<Expr>, Box<Expr>),
+    Lam(Name, Box<Expr>),
+    Var(Name),
+    Lit(Lit),
 }
 
 /// Values
 #[derive(Clone, Debug)]
-pub enum Value{
-    Bar,
+pub enum Value {
+    VNum(Num),
+    VApp(Box<Value>, Box<Value>),
+    VLam(fn(Box<Value>) -> Box<Value>),
 }
 
-/// Types 
+/// Types
 #[derive(Clone, Debug)]
-pub enum Type{
-    Baz,
+pub enum Type {
+    TyVar(Name),
+    TyCon(Name, Vec<Box<Type>>),
+    TyFun(Box<Type>, Box<Type>),
 }
