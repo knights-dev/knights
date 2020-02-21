b13b4abf0b1620577b36626eeba3db48e98d3168
0918nobita <nobita.0918@gmail.com>
2020-02-21 13:31:48 +0900

    :wrench: emotion の string style のために stylelint の設定を修正

--- workspaces/editor/.stylelintrc.js
+++ workspaces/editor/.stylelintrc.js
@@ -1,16 +1,19 @@
 module.exports = {
+    extends: ['stylelint-config-standard'],
     plugins: ['stylelint-scss'],
     rules: {
-        indentation: 4,
-        linebreaks: 'unix',
-        'declaration-block-no-shorthand-property-overrides': true,
-        'declaration-colon-newline-after': null,
         'at-rule-no-unknown': [
             true,
             {
                 ignoreAtRules: ['function', 'if', 'for', 'each', 'include', 'mixin', 'content'],
             },
         ],
+        'declaration-block-no-shorthand-property-overrides': true,
+        'declaration-colon-newline-after': null,
+        'declaration-empty-line-before': null,
+        indentation: 4,
+        linebreaks: 'unix',
+        'no-eol-whitespace': null,
+        'no-missing-end-of-source-newline': null,
     },
-    extends: ['stylelint-config-standard'],
 };
